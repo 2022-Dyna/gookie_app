f1baaebce31c877b9d37caf0eec0280a32ac6b7c
hdy86 <dyhong86@naver.com>
2022-10-27 16:19:44 +0900

    221027_16:19_alarm, pwreset

--- app/Screen/Alarm/index.js
+++ app/Screen/Alarm/index.js
@@ -1,13 +1,177 @@
-import { View, Text, Button } from "react-native";
+import {useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  ScrollView,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import {commonStyles} from '../../common';
 
-export default function Alarm({ navigation }) {
-    return (
-        <View>
-            <View>
-                <Text>
-                    알람페이지 입니다.
-                </Text>
-            </View>
+export default function Alarm({navigation}) {
+  const loginState = {
+    login: true,
+    isCon: false,
+    email: 'ezicland@naver.com',
+    name: '손동윤',
+    userCd: 1,
+  };
+  const data1 = [
+    {
+      alarmId: 1,
+      name: '강기윤',
+      date: '22.10.26',
+      isRead: false,
+    },
+    {
+      alarmId: 2,
+      name: '김대윤',
+      date: '22.10.25',
+      isRead: true,
+    },
+    {
+      alarmId: 3,
+      name: '강민정',
+      date: '22.10.21',
+      isRead: false,
+    },
+  ];
+  const data2 = [
+    {
+      alarmId: 1,
+      name: '강기윤',
+      group: '국민의 힘',
+      date: '22.10.26',
+      isRead: false,
+    },
+    {
+      alarmId: 2,
+      name: '김대윤',
+      group: '국민의 힘',
+      date: '22.10.24',
+      isRead: true,
+    },
+    {
+      alarmId: 3,
+      name: '강민정',
+      group: '더불어민주당',
+      date: '22.10.22',
+      isRead: false,
+    },
+  ];
+
+  const [datas, setDatas] = useState(loginState.isCon ? data1 : data2);
+  // const [datas, setDatas] = useState([]);
+
+  return (
+    <View style={{height: '100%'}}>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View style={styles.alarmTit}>
+          <Text style={styles.alarmTitText}>알림</Text>
         </View>
-    );
+        <FlatList
+          contentContainerStyle={datas.length === 0 && {flex: 1}}
+          ListEmptyComponent={() => {
+            return (
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                }}>
+                <View>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      color: '#7b7b7b',
+                      letterSpacing: -0.24,
+                    }}>
+                    알림이 없습니다.
+                  </Text>
+                </View>
+              </View>
+            );
+          }}
+          empty
+          data={datas}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity
+                activeOpacity={1}
+                onPress={() => {
+                  setDatas(state => {
+                    navigation.navigate('Detail');
+                    const arr = state.filter(el => {
+                      if (el.alarmId == item.alarmId) {
+                        if (!el.isRead) el.isRead = true;
+                      }
+                      return el;
+                    });
+                    return arr;
+                  });
+                }}>
+                <View
+                  style={
+                    item.isRead
+                      ? {paddingHorizontal: 24}
+                      : {paddingHorizontal: 24, backgroundColor: '#fff8f2'}
+                  }>
+                  <View style={styles.alarmBox}>
+                    <View>
+                      {!loginState.isCon && (
+                        <View
+                          style={[commonStyles.mb16, {flexDirection: 'row'}]}>
+                          <View style={commonStyles.mr8}>
+                            <Text style={styles.alarmName}>{item.name}</Text>
+                          </View>
+                          <View>
+                            <Text style={styles.alarmGroup}>{item.group}</Text>
+                          </View>
+                        </View>
+                      )}
+                      <Text style={styles.alarmDesc}>
+                        {loginState.isCon
+                          ? `${item.name} 님이 댓글을 남겼습니다.`
+                          : `${item.name} 의원이 답글을 남겼습니다.`}
+                      </Text>
+                    </View>
+                    <View>
+                      <Text style={styles.alarmDate}>{item.date}</Text>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </ScrollView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  alarmTit: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+  alarmTitText: {
+    fontSize: 16,
+    color: '#313131',
+    fontWeight: 'bold',
+    letterSpacing: -0.32,
+  },
+  alarmBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eeeeee',
+  },
+  alarmName: {fontSize: 14, color: '#454545', letterSpacing: -0.28},
+  alarmGroup: {fonSize: 12, color: '#b1b1b1', letterSpacing: -0.24},
+  alarmDate: {fontSize: 12, color: '#b1b1b1', letterSpacing: -0.24},
+  alarmDesc: {fontSize: 12, color: '#7b7b7b', letterSpacing: -0.24},
+});
--- app/Screen/PwReset/index.js
+++ app/Screen/PwReset/index.js
@@ -7,88 +7,213 @@ import {
   StyleSheet,
   ScrollView,
 } from 'react-native';
+import * as Icons from 'react-native-heroicons/outline';
 import {commonStyles} from '../../common';
 import ConfirmModal from '../../Component/ConfirmModal';
 
-export default function PwReset({navigation}) {
+export default function newPwReset({navigation}) {
+  const loginState = {
+    login: true,
+    isCon: true,
+    email: 'ezicland@naver.com',
+    name: '손동윤',
+    userCd: 1,
+    pw: 'abcd1234', // 비밀번호 임시로 작성
+  };
+
   const [pwValue, setPwValue] = useState('');
-  const [pwConfirmValue, setPwConfirmValue] = useState('');
+  const [newPwValue, setnewPwValue] = useState('');
+  const [newPwConfirmValue, setnewPwConfirmValue] = useState('');
   const [pwMsg, setPwMsg] = useState('');
 
   const [pwFocus, setPwFocus] = useState(false);
-  const [pwConfirmFocus, setPwConfirmFocus] = useState(false);
+  const [newPwFocus, setnewPwFocus] = useState(false);
+  const [newPwConfirmFocus, setnewPwConfirmFocus] = useState(false);
+  const [pwSecret, setPwSecret] = useState(true);
+  const [newPwSecret, setNewPwSecret] = useState(true);
+  const [newPwConfirmSecret, setNewPwConfirmSecret] = useState(true);
   const [completePress, setCompletePress] = useState(false);
   const [modalComplete, setModalComplete] = useState(false);
 
   let disabledComplete = false;
-  pwValue.length !== 0 && pwConfirmValue.length !== 0
+  pwValue.length !== 0 &&
+  newPwValue.length !== 0 &&
+  newPwConfirmValue.length !== 0
     ? (disabledComplete = false)
     : (disabledComplete = true);
 
   const onValid = () => {
-    if (pwValue === pwConfirmValue) {
-      if (pwValue.length < 6 || pwValue.length > 20) {
-        setPwMsg('비밀번호가 너무 짧습니다.');
-      } else if (pwValue.search(/\s/) !== -1) {
-        setPwMsg('공백없이 입력해주세요.');
-      } else if (pwValue.search(/[0-9]/g) < 0 || pwValue.search(/[a-z]/g) < 0) {
-        setPwMsg('영문, 숫자를 혼합하여 입력해주세요.');
+    if (pwValue === loginState.pw) {
+      if (newPwValue === newPwConfirmValue) {
+        if (newPwValue.length < 6 || newPwValue.length > 20) {
+          setPwMsg('비밀번호가 너무 짧습니다.');
+        } else if (newPwValue.search(/\s/) !== -1) {
+          setPwMsg('공백없이 입력해주세요.');
+        } else if (
+          newPwValue.search(/[0-9]/g) < 0 ||
+          newPwValue.search(/[a-z]/g) < 0
+        ) {
+          setPwMsg('영문, 숫자를 혼합하여 입력해주세요.');
+        } else {
+          setModalComplete(true);
+        }
       } else {
-        setModalComplete(true);
+        setPwMsg('변경할 비밀번호가 일치하지 않습니다.');
       }
     } else {
-      setPwMsg('비밀번호가 일치하지 않습니다.');
+      setPwMsg('현재 비밀번호를 다시 확인해주세요.');
     }
   };
 
   return (
     <View>
-      <View style={[commonStyles.inner, styles.basic]}>
+      <View style={styles.pwResetTit}>
+        <Text style={styles.pwResetTitText}>내정보 수정</Text>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingTop: 16,
+          paddingBottom: 32,
+        }}>
         <View>
-          <Text style={[commonStyles.maintit, commonStyles.mb24]}>
-            비밀번호 재설정
-          </Text>
-
-          <ScrollView contentContainerStyle={{paddingBottom: 74}}>
-            <View>
-              <Text style={[commonStyles.labeltext, commonStyles.mb8]}>
-                새 비밀번호
-              </Text>
-              <TextInput
-                style={!pwFocus ? commonStyles.input : commonStyles.inputfocus}
-                name="pw"
-                value={pwValue}
-                onFocus={() => setPwFocus(true)}
-                onBlur={() => setPwFocus(false)}
-                onChange={e => setPwValue(e.nativeEvent.text)}
-                secureTextEntry={true}
-              />
-            </View>
-            <View style={commonStyles.mt8}>
-              <Text style={[commonStyles.labeltext, commonStyles.mb8]}>
-                새 비밀번호 확인
-              </Text>
-              <TextInput
-                style={
-                  !pwConfirmFocus ? commonStyles.input : commonStyles.inputfocus
-                }
-                name="pwConfirm"
-                value={pwConfirmValue}
-                onFocus={() => setPwConfirmFocus(true)}
-                onBlur={() => setPwConfirmFocus(false)}
-                onChange={e => setPwConfirmValue(e.nativeEvent.text)}
-                secureTextEntry={true}
-              />
-              {pwMsg.length !== 0 && (
-                <Text style={[commonStyles.mt8, commonStyles.validText]}>
-                  {pwMsg}
-                </Text>
-              )}
-            </View>
-          </ScrollView>
+          <Text style={[commonStyles.labeltext, commonStyles.mb8]}>이름</Text>
+          <TextInput
+            style={commonStyles.input}
+            name="name"
+            value={loginState.name}
+            editable={false}
+          />
         </View>
+        <View style={commonStyles.mt8}>
+          <Text style={[commonStyles.labeltext, commonStyles.mb8]}>이메일</Text>
+          <TextInput
+            style={commonStyles.input}
+            name="email"
+            value={loginState.email}
+            editable={false}
+          />
+        </View>
+        <View style={commonStyles.mt8}>
+          <Text style={[commonStyles.labeltext, commonStyles.mb8]}>
+            현재 비밀번호
+          </Text>
+          <View style={{position: 'relative'}}>
+            <TextInput
+              style={!pwFocus ? commonStyles.input : commonStyles.inputfocus}
+              name="pw"
+              value={pwValue}
+              onFocus={() => setPwFocus(true)}
+              onBlur={() => setPwFocus(false)}
+              onChange={e => setPwValue(e.nativeEvent.text)}
+              secureTextEntry={pwSecret}
+            />
+            <View
+              style={{
+                position: 'absolute',
+                top: 12,
+                right: 15,
+              }}>
+              <TouchableOpacity
+                activeOpacity={1}
+                onPress={() => setPwSecret(prev => !prev)}>
+                {pwSecret === true ? (
+                  <Icons.EyeSlashIcon
+                    color="#d0d0d0"
+                    fill="transparent"
+                    size={24}
+                  />
+                ) : (
+                  <Icons.EyeIcon color="#d0d0d0" fill="transparent" size={24} />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={commonStyles.mt8}>
+          <Text style={[commonStyles.labeltext, commonStyles.mb8]}>
+            새 비밀번호
+          </Text>
+          <View style={{position: 'relative'}}>
+            <TextInput
+              style={!newPwFocus ? commonStyles.input : commonStyles.inputfocus}
+              name="newPw"
+              value={newPwValue}
+              onFocus={() => setnewPwFocus(true)}
+              onBlur={() => setnewPwFocus(false)}
+              onChange={e => setnewPwValue(e.nativeEvent.text)}
+              secureTextEntry={newPwSecret}
+            />
+            <View
+              style={{
+                position: 'absolute',
+                top: 12,
+                right: 15,
+              }}>
+              <TouchableOpacity
+                activeOpacity={1}
+                onPress={() => setNewPwSecret(prev => !prev)}>
+                {pwSecret === true ? (
+                  <Icons.EyeSlashIcon
+                    color="#d0d0d0"
+                    fill="transparent"
+                    size={24}
+                  />
+                ) : (
+                  <Icons.EyeIcon color="#d0d0d0" fill="transparent" size={24} />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={commonStyles.mt8}>
+          <Text style={[commonStyles.labeltext, commonStyles.mb8]}>
+            새 비밀번호 확인
+          </Text>
+          <View style={{position: 'relative'}}>
+            <TextInput
+              style={
+                !newPwConfirmFocus
+                  ? commonStyles.input
+                  : commonStyles.inputfocus
+              }
+              name="newPwConfirm"
+              value={newPwConfirmValue}
+              onFocus={() => setnewPwConfirmFocus(true)}
+              onBlur={() => setnewPwConfirmFocus(false)}
+              onChange={e => setnewPwConfirmValue(e.nativeEvent.text)}
+              secureTextEntry={newPwConfirmSecret}
+            />
+            <View
+              style={{
+                position: 'absolute',
+                top: 12,
+                right: 15,
+              }}>
+              <TouchableOpacity
+                activeOpacity={1}
+                onPress={() => setNewPwConfirmSecret(prev => !prev)}>
+                {pwSecret === true ? (
+                  <Icons.EyeSlashIcon
+                    color="#d0d0d0"
+                    fill="transparent"
+                    size={24}
+                  />
+                ) : (
+                  <Icons.EyeIcon color="#d0d0d0" fill="transparent" size={24} />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        {pwMsg.length !== 0 && (
+          <Text style={[commonStyles.mt8, commonStyles.validText]}>
+            {pwMsg}
+          </Text>
+        )}
 
-        <View style={styles.btnPos}>
+        <View style={commonStyles.mt56}>
           <TouchableOpacity
             activeOpacity={1}
             disabled={disabledComplete}
@@ -103,39 +228,38 @@ export default function PwReset({navigation}) {
                   ? [commonStyles.btnDefault, commonStyles.btnBgColor]
                   : [commonStyles.btnDefault, commonStyles.btnPressColor]
               }>
-              <Text style={[commonStyles.btnTextDefault]}>재설정</Text>
+              <Text style={[commonStyles.btnTextDefault]}>수정</Text>
             </View>
           </TouchableOpacity>
         </View>
+      </ScrollView>
 
-        {/* modal - 비밀번호 변경 */}
-        <ConfirmModal
-          transparent={true}
-          btnBoolean={modalComplete}
-          onPress={() => {
-            setModalComplete(false);
-            navigation.navigate('Home');
-          }}
-          titleText={'비밀번호 변경 완료'}
-          bodyText={'비밀번호가 변경되었습니다.'}
-          btnText={'확인'}
-        />
-      </View>
+      {/* modal - 비밀번호 변경 완료 */}
+      <ConfirmModal
+        transparent={true}
+        btnBoolean={modalComplete}
+        onPress={() => {
+          setModalComplete(false);
+          navigation.navigate('MyPage');
+        }}
+        titleText={'수정 완료'}
+        bodyText={'내 정보가 수정되었어요.'}
+        btnText={'확인'}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  basic: {
-    position: 'relative',
-    height: '100%',
-    paddingVertical: 48,
-    paddingBottom: 74,
+  pwResetTit: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
   },
-  btnPos: {
-    position: 'absolute',
-    left: 24,
-    bottom: 20,
-    width: '100%',
+  pwResetTitText: {
+    fontSize: 16,
+    color: '#313131',
+    fontWeight: 'bold',
+    letterSpacing: -0.32,
   },
 });
