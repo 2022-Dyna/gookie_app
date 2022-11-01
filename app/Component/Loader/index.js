59a240ca5776a315ee7270b2b2e63997fead238a
hdy86 <dyhong86@naver.com>
2022-11-01 14:25:20 +0900

    221101_14:25_loader

---
+++ app/Component/Loader/index.js
@@ -0,0 +1,47 @@
+import {Image, View} from 'react-native';
+
+export default function Loader(props) {
+  const {type} = props;
+
+  return (
+    // type: full | trans | small
+    <View
+      style={
+        type === 'small'
+          ? {
+              justifyContent: 'center',
+              alignItems: 'center',
+            }
+          : type === 'full'
+          ? {
+              justifyContent: 'center',
+              alignItems: 'center',
+              flex: 1,
+            }
+          : {
+              justifyContent: 'center',
+              alignItems: 'center',
+              position: 'absolute',
+              flex: 1,
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: 'rgba(255, 255, 255, 0)',
+              zIndex: 100,
+            }
+      }>
+      <Image
+        source={
+          type === 'small'
+            ? require('../../img/loading2.gif')
+            : require('../../img/loading.gif')
+        }
+        resizeMode={'contain'}
+        style={
+          type === 'small' ? {width: 50, height: 50} : {width: 100, height: 50}
+        }
+      />
+    </View>
+  );
+}
